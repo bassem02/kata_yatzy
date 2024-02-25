0ac31f37432165501250e336aa8aa2b4d3ad216a
bassem.benamar <bassem.benamar@adservio.fr>
2024-02-25 16:08:00 +0100

    use Junit 5 and refactor the test class using ParameterizedTest and assertJ

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,110 +1,299 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
 
-    @Test
-    public void chance_scores_sum_of_all_dice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
-        assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3,3,4,5,1));
+    @ParameterizedTest
+    @MethodSource("chanceDiceParams")
+    public void chance_category_shouldSumAllDice(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.chance(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
     }
 
-    @Test public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+    static Stream<Arguments> chanceDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(2, 3, 4, 5, 1), 15),
+            Arguments.of(List.of(3, 3, 4, 5, 1), 16)
+        );
     }
 
-    @Test public void test_1s() {
-        assertTrue(Yatzy.ones(1,2,3,4,5) == 1);
-        assertEquals(2, Yatzy.ones(1,2,1,4,5));
-        assertEquals(0, Yatzy.ones(6,2,2,4,5));
-        assertEquals(4, Yatzy.ones(1,2,1,1,1));
+    @ParameterizedTest
+    @MethodSource("yatzyDiceParams")
+    public void yatzy_category_shouldScores50(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.yatzy(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
     }
 
-    @Test
-    public void test_2s() {
-        assertEquals(4, Yatzy.twos(1,2,3,2,6));
-        assertEquals(10, Yatzy.twos(2,2,2,2,2));
+    static Stream<Arguments> yatzyDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(4, 4, 4, 4, 4), 50),
+            Arguments.of(List.of(6, 6, 6, 6, 6), 50),
+            Arguments.of(List.of(4, 6, 6, 6, 3), 0)
+        );
     }
 
-    @Test
-    public void test_threes() {
-        assertEquals(6, Yatzy.threes(1,2,3,2,3));
-        assertEquals(12, Yatzy.threes(2,3,3,3,3));
+    @ParameterizedTest
+    @MethodSource("OnesDiceParams")
+    public void ones_category_shouldSumOnes(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.ones(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
     }
 
-    @Test
-    public void fours_test() 
-    {
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
+    static Stream<Arguments> OnesDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(1, 2, 1, 4, 5), 2),
+            Arguments.of(List.of(6, 2, 2, 4, 5), 0),
+            Arguments.of(List.of(1, 2, 1, 1, 1), 4)
+        );
     }
 
-    @Test
-    public void fives() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
+    @ParameterizedTest
+    @MethodSource("twosDiceParams")
+    public void twos_category_shouldSumTwos(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.twos(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
     }
 
-    @Test
-    public void sixes_test() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
+    static Stream<Arguments> twosDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(1, 2, 3, 2, 6), 4),
+            Arguments.of(List.of(2, 2, 2, 2, 2), 10)
+        );
     }
 
-    @Test
-    public void one_pair() {
-        assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
-        assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
-        assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
+    @ParameterizedTest
+    @MethodSource("threesDiceParams")
+    public void threes_category_shouldSumTwos(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.threes(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> threesDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(1, 2, 3, 2, 3), 6),
+            Arguments.of(List.of(2, 3, 3, 3, 3), 12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("foursDiceParams")
+    public void fours_category_shouldSumTwos(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = new Yatzy(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4)).fours();
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> foursDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(4, 4, 4, 5, 5), 12),
+            Arguments.of(List.of(4, 4, 5, 5, 5), 8),
+            Arguments.of(List.of(4, 5, 5, 5, 5), 4)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("fivesDiceParams")
+    public void fives_category_shouldSumTwos(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = new Yatzy(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4)).fives();
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> fivesDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(4, 4, 4, 5, 5), 10),
+            Arguments.of(List.of(4, 4, 5, 5, 5), 15),
+            Arguments.of(List.of(4, 5, 5, 5, 5), 20)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sixesDiceParams")
+    public void sixes_category_shouldSumTwos(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = new Yatzy(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4)).sixes();
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> sixesDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(4, 4, 4, 5, 5), 0),
+            Arguments.of(List.of(4, 4, 6, 5, 5), 6),
+            Arguments.of(List.of(6, 5, 6, 6, 5), 18)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("onePairDiceParams")
+    public void onePair_category_shouldSumHighestPair(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.score_pair(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> onePairDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(3, 4, 3, 5, 6), 6),
+            Arguments.of(List.of(5, 3, 3, 3, 5), 10),
+            Arguments.of(List.of(5, 3, 6, 6, 5), 12)
+        );
     }
 
     @Test
     public void two_Pair() {
-        assertEquals(16, Yatzy.two_pair(3,3,5,4,5));
-        assertEquals(16, Yatzy.two_pair(3,3,5,5,5));
+        assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5));
+        assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5));
     }
 
-    @Test
-    public void three_of_a_kind() 
-    {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
+    @ParameterizedTest
+    @MethodSource("twoPairDiceParams")
+    public void twoPairs_category_shouldSumTheTwoPairs(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.two_pair(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
     }
 
-    @Test
-    public void four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
-        assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
+    static Stream<Arguments> twoPairDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(3, 3, 5, 4, 5), 16),
+            Arguments.of(List.of(3, 3, 5, 5, 5), 16)
+        );
     }
 
-    @Test
-    public void smallStraight() {
-        assertEquals(15, Yatzy.smallStraight(1,2,3,4,5));
-        assertEquals(15, Yatzy.smallStraight(2,3,4,5,1));
-        assertEquals(0, Yatzy.smallStraight(1,2,2,4,5));
+    @ParameterizedTest
+    @MethodSource("threeOfAKindDiceParams")
+    public void threeOfAKind_category_shouldSumTheDiceThreeTimes(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.three_of_a_kind(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
     }
 
-    @Test
-    public void largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6,2,3,4,5));
-        assertEquals(20, Yatzy.largeStraight(2,3,4,5,6));
-        assertEquals(0, Yatzy.largeStraight(1,2,2,4,5));
+    static Stream<Arguments> threeOfAKindDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(3, 3, 3, 4, 5), 9),
+            Arguments.of(List.of(5, 3, 5, 4, 5), 15),
+            Arguments.of(List.of(3, 3, 3, 3, 5), 9)
+        );
     }
 
-    @Test
-    public void fullHouse() {
-        assertEquals(18, Yatzy.fullHouse(6,2,2,2,6));
-        assertEquals(0, Yatzy.fullHouse(2,3,4,5,6));
+    @ParameterizedTest
+    @MethodSource("fourOfAKindDiceParams")
+    public void fourOfAKind_category_shouldSumTheDiceFourTimes(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.four_of_a_kind(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> fourOfAKindDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(3, 3, 3, 3, 5), 12),
+            Arguments.of(List.of(5, 5, 5, 4, 5), 20),
+            Arguments.of(List.of(3, 3, 3, 3, 3), 12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("smallStraightDiceParams")
+    public void smallStraight_category_shouldScores15(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.smallStraight(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> smallStraightDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(1, 2, 3, 4, 5), 15),
+            Arguments.of(List.of(2, 3, 4, 5, 1), 15),
+            Arguments.of(List.of(1, 2, 2, 4, 5), 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("largeStraightDiceParams")
+    public void largeStraight_category_shouldScores20(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.largeStraight(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> largeStraightDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(6, 2, 3, 4, 5), 20),
+            Arguments.of(List.of(2, 3, 4, 5, 6), 20),
+            Arguments.of(List.of(1, 2, 2, 4, 5), 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("fullHouseDiceParams")
+    public void fullHouse_category_shouldSumAll(List<Integer> dice, int expectedScore) {
+        //Given
+        //When
+        int sum = Yatzy.fullHouse(dice.get(0), dice.get(1), dice.get(2), dice.get(3), dice.get(4));
+
+        //Then
+        assertThat(sum).isEqualTo(expectedScore);
+    }
+
+    static Stream<Arguments> fullHouseDiceParams() {
+        return Stream.of(
+            Arguments.of(List.of(6, 2, 2, 2, 6), 18),
+            Arguments.of(List.of(2, 3, 4, 5, 6), 0)
+        );
     }
 }
